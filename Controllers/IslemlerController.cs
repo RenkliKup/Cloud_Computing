3da44d3e758d910f8553a48f17f2c6e2fbbde50b
Baris Karakulak <baris.1907.krklk@gmail.com>
2021-05-05 23:02:23 +0300

    change controllers

---
+++ Controllers/IslemlerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class IslemlerController : Controller
+    {
+        // GET: Islemler
+        SQLProjeEntities db = new SQLProjeEntities();
+        
+        public ActionResult Index()
+        {
+            var degerler = db.islemler.ToList();
+            return View(degerler);
+        }
+    }
+}
